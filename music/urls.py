a24c5ac4056c775df9074c40f093ff616256bfbb
m7md3omer <mohammedomer789@gmail.com>
2019-02-23 01:19:12 +0200

    added a favorite attribute to the Song model

--- music/urls.py
+++ music/urls.py
@@ -5,5 +5,10 @@ app_name = 'music'
 urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
+
+    # /music/<album_id>/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name="detail"),
+
+    # /music/<album_id>/
+    url(r'^(?P<album_id>[0-9]+)/favorite$', views.detail, name="detail"),
 ]
