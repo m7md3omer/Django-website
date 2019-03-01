537b09dd55714a86d54cfe0f5b25d5bf0756fd9e
m7md3omer <mohammedomer789@gmail.com>
2019-03-01 20:02:56 +0200

    Uploading images from the pc and making user registration page

--- music/views.py
+++ music/views.py
@@ -1,6 +1,11 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
 from .models import Album
+from .forms import UserForm
 
 
 class IndexView(generic.ListView):
@@ -17,6 +22,52 @@ class DetailView(generic.DetailView):
 
 
 class AlbumCreate(CreateView):
-    template_name = 'music/album_form.html'
     model = Album
     fields = ['artist', 'album_name', 'genre', 'album_logo']
+
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'album_name', 'genre', 'album_logo']
+
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
+
+
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'music/registration_form.html'
+
+    # display a blank form (for get request)
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    # process form data here (the user clicks the submit button)
+    def post(self, request):
+        # add the data submitted by the user as an argument to create the filled form
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            # store the data the user has entered without saving it to the database
+            user = form.save(commit=False)
+
+            # cleaned (normalized data)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            # do not use user.password because it is hashed and will cause errors
+            user.set_password(password)
+            user.save()
+
+            # returns user object if credentials are correct
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('music:index')
+        return render(request, self.template_name, {'form': form})
+
+
--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.conf.urls import url, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'website'
 
@@ -9,3 +11,8 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'music/', include('music.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
